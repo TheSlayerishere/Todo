c95b2987f18ee63245281d7599c952e5133cfe19
Harshil Borthakur <ghouldrago0@gmail.com>
2021-02-13 12:41:49 +0530

    MORE AUTOFORMATTING

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -10,17 +10,19 @@ namespace Todo.ViewModels
         public MainWindowViewModel(DataBase db)
         {
             Content = List = new TodoListViewModel(db.GetItems());
-            
+
 
         }
-        public ViewModelBase Content{
+        public ViewModelBase Content
+        {
             get => content;
             private set => this.RaiseAndSetIfChanged(ref content, value);
         }
 
-        public TodoListViewModel List {get;}
-        public void AddItem(){
+        public TodoListViewModel List { get; }
+        public void AddItem()
+        {
             Content = new AddItemViewModel();
-        } 
-    }   
+        }
+    }
 }
